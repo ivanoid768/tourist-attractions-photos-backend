03adef08edef66186137d0919b0e713da590dedb
ivanoid256 <ivanoid256@india.com>
2019-05-09 14:38:56 +0500

    graphql init

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const express = require('express')
 const axios = require('axios')
 const qs = require('qs')
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const app = express()
 const port = process.env.PORT || 4000;
+
+// Some fake data
+const books = [
+	{
+		title: "Harry Potter and the Sorcerer's stone",
+		author: 'J.K. Rowling',
+	},
+	{
+		title: 'Jurassic Park',
+		author: 'Michael Crichton',
+	},
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+	type Query { books: [Book] }
+	type Book { title(id: String = "0"): String, author: String }
+  `;
+
+// The resolvers
+const resolvers = {
+	Query: { books: () => books },
+	Book: {
+		title: function (parent, args) {
+			console.log(args)
+			return parent.title;
+		}
+	}
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 const api_key = 'LF7U6m6DmxNiw3JrNV3Q8PjITsukfXrihnHriEcyQd5nlbJH8VfbHPVI3PEAmHzZ';
 const api_secret = 'abLsVWn1oSNXeXmrh7i8x29zt4YhxszoevnwZUqAIvIgmwziZ07EjObiU9UC7NTd';
